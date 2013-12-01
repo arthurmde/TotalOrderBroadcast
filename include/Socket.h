9eeba8b37da76696a1cf414ee0dfd12d795ee4c6
Arthur Del Esposte <arthurmde@yahoo.com.br>
2013-12-01 11:44:32 -0200

    Refactoring for c++ style

--- include/Socket.h
+++ include/Socket.h
@@ -12,41 +12,37 @@
 #include <string>
 #include <arpa/inet.h>
 
-
 const int MAXHOSTNAME = 200;
 const int MAXCONNECTIONS = 5;
 const int MAXRECV = 500;
 
 class Socket
 {
- public:
-  Socket();
-  virtual ~Socket();
+  public:
+    Socket();
+    virtual ~Socket();
 
-  // Server initialization
-  bool create();
-  bool bind ( const int port );
-  bool listen() const;
-  bool accept ( Socket& ) const;
+    // Server initialization
+    bool create();
+    bool bind(const int port);
+    bool listen() const;
+    bool accept(Socket&) const;
 
-  // Client initialization
-  bool connect ( const std::string host, const int port );
+    // Client initialization
+    bool connect(const std::string host, const int port);
 
-  // Data Transimission
-  bool send ( const std::string ) const;
-  int recv ( std::string& ) const;
-
-
-  void set_non_blocking ( const bool );
-
-  bool is_valid() const { return m_sock != -1; }
-
- private:
-
-  int m_sock;
-  sockaddr_in m_addr;
+    // Data Transimission
+    bool send(const std::string) const;
+    int recv(std::string&) const;
 
 
+    void set_non_blocking(const bool);
+
+    bool is_valid() const { return m_sock != -1; }
+
+  private:
+    int m_sock;
+    sockaddr_in m_addr;
 };
 
 
